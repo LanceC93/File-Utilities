0d19dbd7d3dae23f9638f8b1d77d72e6fd768520
LanceC93 <42276366+LanceC93@users.noreply.github.com>
2022-07-03 12:36:03 -0500

    Update extractor.py
    added case for no file selection

--- extractor.py
+++ extractor.py
@@ -1,13 +1,25 @@
 #extracts multiple zips to a destination
+import time
 from zipfile  import ZipFile
 from tkinter import filedialog
 
 #asks for what zip files need to be extracted
+print('Select zip files:')
+time.sleep(.5)
 zipped_files = filedialog.askopenfilenames(title='Which Files to Unzip', filetypes=[('Zipped Folders', ".zip")])
 
-#asks where to extract files to
-target_directory = filedialog.askdirectory(title='Unzip Destination')
+if not zipped_files:
+    print("No zip files selected.")
+else:
+    #asks where to extract files to
+    print("Select Destination:")
+    time.sleep(.5)
+    target_directory = filedialog.askdirectory(title='Unzip Destination')
 
-#extracts each zip to target
-for file in zipped_files:
-    ZipFile(file).extractall(target_directory)
+    if target_directory != "":
+        #extracts each zip to target
+        for file in zipped_files:
+            ZipFile(file).extractall(target_directory)
+        print('Extracting Complete')
+    else:
+        print('No Destination Selected')
